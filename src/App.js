25d19ec7c7b9241fbda9d30b17ead877a03a796e
Pankaj Pandey <153015921+Pankaj2822004@users.noreply.github.com>
2024-05-05 03:10:33 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ function App() {
               element={
                 <Textform
                   heading={
-                    "MYWEBS - character count , wordcount , cleartext, copytext , remove Extra spaces"
+                    "MYWEB - character count , wordcount , cleartext, copytext , remove Extra spaces"
                   }
                   mode={mode}
                   showAlert={showAlert}
